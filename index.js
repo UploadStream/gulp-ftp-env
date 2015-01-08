d842f09a2e1601afdcf360818e0f265de1da8758
Wayne <dev.wayne.ix@gmail.com>
2015-01-08 18:26:02 +0800

    version

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 var jsftp = require('jsftp'),
     log = require('clilog'),
+    path = require('path'),
     through = require('through2'),
     util = require('gulp-util');
 
@@ -8,25 +9,30 @@ var JSFtp = require('jsftp-mkdirp')(jsftp);
 
 module.exports = function(options) {
     var fileCount = 0;
+	var version=new Date().getTime().toString();
     if (options.host == undefined) {
         throw new util.PluginError('gulp-ftp-env', '`host` required.');
     }
     return through.obj(function(file, enc, cb) {
+        if (file.isNull()) {
+            this.push(file);
+            return cb();
+        }
+
         var self = this;
 
         var ftp = new JSFtp(options);
-
-        var finalRemotePath = path.join('/', options.remotePath, file.relative).replace(/\\/g, '/');
-
+        var finalRemotePath = path.join('/', options.remotePath,version, file.relative).replace(/\\/g, '/');
+	    console.log(file.relative);
         ftp.mkdirp(path.dirname(finalRemotePath).replace(/\\/g, '/'), function(err) {
             if (err) {
-                self.emit('error', new gutil.PluginError('gulp-ftp-env', err));
+                self.emit('error', new util.PluginError('gulp-ftp-env', err));
                 return cb();
             }
 
             ftp.put(file.contents, finalRemotePath, function(err) {
                 if (err) {
-                    self.emit('error', new gutil.PluginError('gulp-ftp-env', err));
+                    self.emit('error', new util.PluginError('gulp-ftp-env', err));
                     return cb();
                 }
 
@@ -39,9 +45,9 @@ module.exports = function(options) {
         this.push(file);
     }, function(cb) {
         if (fileCount > 0) {
-            gutil.log('gulp-ftp-env:', gutil.colors.green(fileCount, fileCount === 1 ? 'file' : 'files', 'uploaded successfully'));
+            util.log('gulp-ftp-env:', util.colors.green(fileCount, fileCount === 1 ? 'file' : 'files', 'uploaded successfully'));
         } else {
-            gutil.log('gulp-ftp-env:', gutil.colors.yellow('No files uploaded'));
+            util.log('gulp-ftp-env:', util.colors.yellow('No files uploaded'));
         }
 
         cb();
